69dceb4d1b740d5a43414065e99d13cb07dbb133
StevenAwo <rotimiawomodu@gmail.com>
2023-11-29 16:26:49 +0100

    TASK 4

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  * Return: If node is actually a leaf - 1.
  *         Otherwise - 0.
  */
- 
+
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL || node->left != NULL || node->right != NULL)
