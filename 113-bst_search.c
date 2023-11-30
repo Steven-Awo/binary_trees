a2dc776a463b8e92d9a32d1b3de3600a1a20316e
StevenAwo <rotimiawomodu@gmail.com>
2023-11-30 15:48:13 +0100

    TASK 27

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -19,7 +19,7 @@ bst_t *bst_search(const bst_t *tree, int value)
 	return ((bst_t *)tree);
 	}
 	if (tree->n > value)
-	{	
+	{
 	return (bst_search(tree->left, value));
 	}
 	return (bst_search(tree->right, value));
